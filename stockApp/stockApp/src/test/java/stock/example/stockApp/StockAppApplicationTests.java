5456d60dd1bf58595f4f652e038c69c5a2e93c35
Amit Nayek <amit.nayek@incedoinc.com>
2022-01-10 21:53:58 +0530

    initial commit

---
+++ stockApp/stockApp/src/test/java/stock/example/stockApp/StockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package stock.example.stockApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
